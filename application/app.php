88f1fbd005c07c69f3f8c4db63485a5f835917e5
Tran Thanh Tu <tutran@tctav.com>
2015-01-13 10:54:32 +0700

    initiate application instance, load controller after parsing url; update Loader.php
    , add callController function

--- application/app.php
+++ application/app.php
@@ -5,15 +5,32 @@ require_once '/../library/loader.php';
 
 class Application
 {
+	private $url;
+	private $loader;
+	private $controller = "defaultController";
+	private $method = "defaultMethod";
+	private $arrParams = array();
+
+	function __construct()
+	{
+		$this->loader = new Loader;
+	}
+
+	function parseURL() 
+	{
+		$arrQueryString = explode('&', $_SERVER['QUERY_STRING']);
+		$arrParseResult = array_diff(explode('/', $arrQueryString[0]), array(''));
+		if(count($arrParseResult) >= 1) $this->controller = $arrParseResult[0];
+		if(count($arrParseResult) >= 2) $this->method = $arrParseResult[1];
+		if(count($arrParseResult) >= 3) $this->arrParams = array_slice($arrParseResult, 2);
+	}
+
 	function run() 
 	{
-		echo 'test run' . '<br>';
-		echo PROJECT_FOLDER . '<br>';
-		echo APPLICATION_DIR . '<br>';
-		echo CONTROLLERS_DIR . '<br>';		
+		$this->parseURL();
+		$this->loader->callController($this->controller, $this->method, $this->arrParams);
 	}
 }
 
 $app = new Application;
 $app->run();
-
--- library/Loader.php
+++ library/Loader.php
@@ -4,16 +4,48 @@ require_once '/../config/define.php';
 
 class Loader 
 {
-	function loadModel($model)
+	public function loadModel($model = "")
 	{
-		$model_dir = MODELS_DIR . $model . '.php';
-		if(file_exists($model_dir)) {
-			require_once ($model_dir);
+		$modelDir = MODELS_DIR . $model . '.php';
+		if(file_exists($modelDir)) {
+			require_once ($modelDir);
 			$objTempModel = new $model;
 			return $objTempModel;
 		}
 		else {
 			return null;
 		}
+	}
+
+	public function loadController($controller = "") {
+		$controllerDir = CONTROLLERS_DIR . $controller . '.php';
+		if(file_exists($controllerDir)) {
+			require_once($controllerDir);
+			return $controllerDir;
+		}
+		else {
+			return null;
+		}
+	}
+
+	public function loadView($view = "") {
+		$viewDir = VIEWS_DIR . $view . '.php';
+		if(file_exists($viewDir)) {
+			require_once($viewDir);	
+		}
+		else {
+			$this->loadView('404');
+		}
+	}
+
+	public function callController($controller = "", $method = "", $params)
+	{
+		if($this->loadController($controller) != null) {
+			$objTempClass = new $controller;
+			call_user_func_array(array($objTempClass, $method), $params);		
+		} else {
+			$this->loadView('404');
+		}
+
 	}
 }
